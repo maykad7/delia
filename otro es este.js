d88685a7ccc1271a8180ddf33016f81ced0fd38b
mayka <you@gmail.com>
2020-03-12 21:15:32 -0400

    Este commit esta enco en el login Otro

---
+++ otro es este.js
@@ -0,0 +1,11 @@
+el camino no esta temsinado 
+git branch  sirve para ver cuantas login hay muestra la lista
+git branch nombreDelNuevoLogin este se para crear otro login
+git checkout nombreLogin >. este sirve para cambiarce de login
+
+git log te muestra los commit que se hizo
+git status   te muestra los cambios que se esta haciendo en el proyecto antes de ser aniadios
+git add nombre_archivo   este comando sirve para aceptar los cambios de un archivo
+git add .   este commando sirve para aceptar los cambios en todos lor archivos que precentes cambios.
+git restore  masElNombreDelArchivo sirve para restaurar el archivos astes de usar el comando "git add"
+git commit    este comando sirve para aseptar los cambios de un archivos o de varios.
